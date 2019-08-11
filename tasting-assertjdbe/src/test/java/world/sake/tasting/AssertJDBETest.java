0fcdd0cd4561be4c9495f7a5d11c0d763fca21af
mito kunihiro <k-mito@unext.jp>
2019-08-11 17:27:13 +0900

    no message

--- tasting-assertjdbe/src/test/java/world/sake/tasting/AssertJDBETest.java
+++ tasting-assertjdbe/src/test/java/world/sake/tasting/AssertJDBETest.java
@@ -1,12 +1,29 @@
 package world.sake.tasting;
 
 import javax.annotation.Resource;
+import javax.sql.DataSource;
 
+import org.lastaflute.core.util.ContainerUtil;
+
+import world.sake.assertjdbe.AssertDBEquals;
+import world.sake.assertjdbe.ExpectedWorkbook;
 import world.sake.dbflute.exbhv.NihonshuBhv;
 import world.sake.dbflute.shochudb.exbhv.ShochuBhv;
 import world.sake.unit.UnitAssertjdbeTestCase;
 
 public class AssertJDBETest extends UnitAssertjdbeTestCase {
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        AssertDBEquals jdbe = new AssertDBEquals(ContainerUtil.searchComponentsAll(DataSource.class)) {
+            @Override
+            protected ExpectedWorkbook createExpectedWorkbook() {
+                return super.createExpectedWorkbook();
+            }
+        };
+        log(jdbe.toString());
+    }
 
     @Resource
     private NihonshuBhv nihonshuBhv;
